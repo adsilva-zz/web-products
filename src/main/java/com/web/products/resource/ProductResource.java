dda2a15effd25ccf2053f645a32f1a3fc1326498
Amanda Ito <adsilva32@gmail.com>
2020-04-02 19:57:19 -0300

    Create resource Product

--- src/main/java/com/web/products/resource/ProductResource.java
+++ src/main/java/com/web/products/resource/ProductResource.java
@@ -1,18 +1,28 @@
 package com.web.products.resource;
 
 import com.web.products.model.dto.ProductDto;
+import com.web.products.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
-@RequestMapping("/products")
+@RequestMapping("api/v1/products")
 public class ProductResource {
 
+    private ProductService productService;
+
+    public ProductResource(ProductService productService) {
+        this.productService = productService;
+    }
+
     @PostMapping
-    public ResponseEntity<ProductDto> createProduct(){
-        return new ResponseEntity(HttpStatus.CREATED);
+    public ResponseEntity<ProductDto> createProduct(@RequestBody @Valid ProductDto productDto) {
+        return new ResponseEntity(productService.createProduct(productDto), HttpStatus.CREATED);
     }
 }
