c1b71adc3251467ea78cb84974235461b3286d42
Amanda Ito <adsilva32@gmail.com>
2020-04-02 19:56:31 -0300

    Create converts dto

--- src/main/java/com/web/products/converts/ProductDtoToProduct.java
+++ src/main/java/com/web/products/converts/ProductDtoToProduct.java
@@ -1,4 +1,16 @@
 package com.web.products.converts;
 
-public class ProductDtoToProduct {
+import com.web.products.model.Product;
+import com.web.products.model.dto.ProductDto;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductDtoToProduct implements Converter<ProductDto, Product> {
+
+    @Override
+    public Product convert(ProductDto productDto) {
+        Product product = Product.builder().name(productDto.getName()).build();
+        return product;
+    }
 }
